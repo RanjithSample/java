9416082032edf6acba645aa0f1311b763256f49d
Ranjithkumar S <ranjithkumar60304@gmail.com>
2022-10-04 13:18:58 +0530

    first commit

---
+++ Args.java
@@ -0,0 +1,11 @@
+
+public class Args{
+	public static void main(String args[]){
+		System.out.println("Hiiiiiiiiiiiiiiiiii");
+		System.out.println(args[0]);
+	}
+}
+
+//---->java Args.java hii
+//Hiiiiiiiiiiiiiiiiii
+//hii
---
+++ app.js
@@ -0,0 +1,11 @@
+let a;
+let b;
+/*console.log("This is log");
+console.warn("This is warnning");
+console.info("This is information");
+console.error("This is error");
+*/
+a=prompt("enter a");
+b=prompt("enter b");
+
+console.log(a+b);
